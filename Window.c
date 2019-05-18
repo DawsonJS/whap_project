23187f0f40a5a27b48cc44455fae530e22fcd7b2
DawsonJS <dschraiber1@gmail.com>
2019-05-18 18:44:07 -0500

    New Sprite/Animations

--- Window.c
+++ Window.c
@@ -11,35 +11,49 @@ int main()
 		int walkRight;
 		int walkLeft;
 		int walkCount;
+		int standCount;
 	};
-	struct charAspects man = {TRUE, FALSE, FALSE, 0};
+	struct charAspects man = {TRUE, TRUE, FALSE, 0, 0};
     int screenWidth = 480;
     int screenHeight = 272;
     InitWindow(screenWidth, screenHeight, VERSION);
-    Texture2D charLeft[9] = {LoadTexture("images/L1.png"), LoadTexture("images/L2.png"), LoadTexture("images/L3.png"),
-							LoadTexture("images/L4.png"), LoadTexture("images/L5.png"), LoadTexture("images/L6.png"), 
-							LoadTexture("images/L7.png"), LoadTexture("images/L8.png"), LoadTexture("images/L9.png")};
-	Texture2D charRight[9] = {LoadTexture("images/R1.png"), LoadTexture("images/R2.png"), LoadTexture("images/R3.png"),
-							LoadTexture("images/R4.png"), LoadTexture("images/R5.png"), LoadTexture("images/R6.png"), 
-							LoadTexture("images/R7.png"), LoadTexture("images/R8.png"), LoadTexture("images/R9.png")};
+    Texture2D charLeft[6] = {LoadTexture("images/runleft00.png"), LoadTexture("images/runleft01.png"), LoadTexture("images/runleft02.png"),
+							LoadTexture("images/runleft03.png"), LoadTexture("images/runleft04.png"), LoadTexture("images/runleft05.png")};
+	Texture2D charRight[6] = {LoadTexture("images/runright00.png"), LoadTexture("images/runright01.png"), LoadTexture("images/runright02.png"),
+							LoadTexture("images/runright03.png"), LoadTexture("images/runright04.png"), LoadTexture("images/runright05.png")};
 	Texture2D background = LoadTexture("images/background.png");
-	Texture2D character = LoadTexture("images/standing.png");
-	int posX = 50;
+	Texture2D idleRight[3] = {LoadTexture("images/idleright00.png"), LoadTexture("images/idleright01.png"), LoadTexture("images/idleright02.png")};
+	Texture2D idleLeft[3] = {LoadTexture("images/idleleft00.png"), LoadTexture("images/idleleft01.png"), LoadTexture("images/idleleft02.png")};
+	int posX = 0;
 	int posY = 208;
     int vel = 4;
     
     SetTargetFPS(30);
     
-    int windowDraw(struct charAspects charAspects, Texture2D charLeft[], Texture2D charRight[], Texture2D character, int posX, int posY, Texture2D background)
+    struct charAspects windowDraw(struct charAspects charAspects, Texture2D charLeft[], Texture2D charRight[], Texture2D idleRight[], Texture2D idleLeft[], int posX, int posY, Texture2D background)
     {
 		DrawTexture(background, 0, 0, WHITE);
-		if ((charAspects.walkCount + 1) >= 27)
+		DrawFPS(0, 0);
+		if ((charAspects.walkCount + 1) >= 18)
 			{
 			charAspects.walkCount = 0;
 			}
+		if ((charAspects.standCount + 1) >= 18)
+			{
+			charAspects.standCount = 0;
+			}
 		if (charAspects.isStanding == TRUE)
 			{
-			DrawTexture(character, posX, posY, WHITE);
+			if (charAspects.walkRight == TRUE)
+				{
+				DrawTexture(idleRight[charAspects.standCount / 6], posX, posY, WHITE);
+				charAspects.standCount += 1;
+				}
+			if (charAspects.walkLeft == TRUE)
+				{
+				DrawTexture(idleLeft[charAspects.standCount / 6], posX, posY, WHITE);
+				charAspects.standCount += 1;
+				}
 			}
 		else {
 			if (charAspects.walkRight == TRUE)
@@ -63,33 +77,33 @@ int main()
 				}
 				}
 			}
-		return charAspects.walkCount;
+		return charAspects;
 	}
     
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
-        if (IsKeyDown(KEY_RIGHT) && posX < (screenWidth - 64 - vel))
+        if (IsKeyDown(KEY_RIGHT) && posX < (screenWidth - 37 - vel))
 		{
 			posX += vel;
 			man.walkRight = TRUE;
 			man.walkLeft = FALSE;
 			man.isStanding = FALSE;
 		}
-		if (IsKeyDown(KEY_LEFT) && posX > vel)
+		if (IsKeyDown(KEY_LEFT) && posX > 0)
 		{
 			posX -= vel;
 			man.walkRight = FALSE;
 			man.walkLeft = TRUE;
 			man.isStanding = FALSE;
 		}
-		if (IsKeyPressed(KEY_DOWN))
+		if (IsKeyUp(KEY_RIGHT) && IsKeyUp(KEY_LEFT))
 		{
 			man.isStanding = TRUE;
 		}
         
         BeginDrawing();
-		ClearBackground(RAYWHITE);
-		man.walkCount = windowDraw(man, charLeft, charRight, character, posX, posY, background);
+		//ClearBackground(RAYWHITE);
+		man = windowDraw(man, charLeft, charRight, idleRight, idleLeft, posX, posY, background);
         EndDrawing();
     }
 	CloseWindow();
